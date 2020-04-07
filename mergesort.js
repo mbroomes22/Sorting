7d38ce383bbdcc7101c6e791f09bff7d38a31cce
Michelle <michelle_broomes@yahoo.com>
2020-04-07 16:56:15 -0400

    completed mergeSort

--- mergesort.js
+++ mergesort.js
@@ -8,13 +8,11 @@ function split(wholeArray) {
   return [firstHalf, secondHalf];
 }
 
-function merge([arrA= [], arrB= []]) {
+function merge(arr1, arr2) {
   let result = [];
   let i = 0;
   let j = 0;
-  console.log('1',arrA, '2',arrB);
-  let arr1 = arrA;
-  let arr2 = arrB;
+  
   while(i < arr1.length && j < arr2.length) {
     if(arr1[i] < arr2[j]) {
       result.push(arr1[i]);
@@ -32,10 +30,15 @@ function merge([arrA= [], arrB= []]) {
     result.push(arr2[j]);
     j++
   }
+  
   return result;
 }
 
 function mergeSort(arr) {
   if(arr.length <= 1) return arr;
-  return merge(split(arr));
+  let half = Math.floor(arr.length / 2);
+  let firstHalf = mergeSort(arr.slice(0, half));
+  let secondHalf = mergeSort(arr.slice(half));
+
+  return merge(firstHalf, secondHalf);
 }
