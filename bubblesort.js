6d6b4abb91ea74596cd3f76f330aa6d7bb7c9fb2
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-07 13:03:31 -0400

    WIP - test cases

--- bubblesort.js
+++ bubblesort.js
@@ -1,16 +1,19 @@
-
 const bubbleSort = (array) => {
-   if (array.length <= 1) return array;
+  if (array.length <= 1) return array;
 
-   for (let i = 0; i<array.length; i++) {
-       for (let j = 0; j < array.length; j++) {
-           if (array[j] > array[i]) {
-               let biggerNumber =  array[j]
-               array[j] = array[i]
-               array[i] = biggerNumber
-           }
-       }
-   }
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[i]) {
+        swap(arr[j], arr[i]);
+      }
+    }
 
-   return array 
-}
+    return array;
+  }
+};
+
+const swap = (val1, val2) => {
+  let biggerNumber = val2;
+  val2 = val1;
+  val1 = biggerNumber;
+};
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,12 +1,15 @@
-describe('Bubble Sort', function(){
-    it('handles an empty array', function(){
-      expect( bubbleSort([]) ).toEqual( [] );
-    });
-    it('handles a single array', function(){
-        expect( bubbleSort([1]) ).toEqual( [1] );
-    });
-    it('handles an array with multiple items', function(){
-        expect( bubbleSort([1, 6, 5, 3, 7]) ).toEqual( [1, 3, 5, 6,7] );
-    });
-
+describe("Bubble Sort", function() {
+  beforeEach(function() {
+    spyOn(swap()).and.callThrough();
   });
+  it("handles an empty array", function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it("handles a single array", function() {
+    expect(bubbleSort([1])).toEqual([1]);
+  });
+  it("handles an array with multiple items", function() {
+    expect(bubbleSort([1, 6, 5, 3, 7])).toEqual([1, 3, 5, 6, 7]);
+    expect(swap.calls.count).toEqual(3);
+  });
+});
